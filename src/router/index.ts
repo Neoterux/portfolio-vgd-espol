5ba414e362562f391b222a8cfbcaf40e34f57ed3
Neoterux <labfernandez2014@gmail.com>
2023-10-22 23:16:05 -0500

    Configurations and utilities

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -23,7 +23,7 @@ const router = createRouter({
     {
         path: '/matrix',
         name: 'matrix',
-        component: () => import('../views/IdeaMatrix.vue'),
+        component: () => import('@views/IdeaMatrix.vue'),
     },
   ]
 })
